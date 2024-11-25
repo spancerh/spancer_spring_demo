7abf773b71f9e8b866c828353ffbc8ecd71cf2d8
Spencer Hsieh <66104491+spancerh@users.noreply.github.com>
2024-11-25 11:05:36 +0800

    Add HelloController

---
+++ src/main/java/com/example/spancer_spring_demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.spancer_spring_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	/*@GetMapping("/")*/
+  @RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
