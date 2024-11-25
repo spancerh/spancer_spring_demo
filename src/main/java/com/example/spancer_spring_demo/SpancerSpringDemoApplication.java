f37545fecc69a5854272734206c82949c15104ae
Spencer Hsieh <66104491+spancerh@users.noreply.github.com>
2024-11-25 10:21:14 +0800

    second commit

---
+++ src/main/java/com/example/spancer_spring_demo/SpancerSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spancer_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpancerSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpancerSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spancer_spring_demo/SpancerSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spancer_spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpancerSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
